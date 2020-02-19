67069cb2489404159d6b1fc7731ca12bd4700fba
Michael Fladischer <michael@fladi.at>
2020-02-19 12:54:42 +0100

    Improve frontend UI.

--- src/outpost/django/borg/static/borg/detail.js
+++ src/outpost/django/borg/static/borg/detail.js
@@ -1,4 +1,12 @@
 $(function() {
+  var steps = $('#tutorial .tab-pane').length;
+  $('#tutorial .progress-bar').attr("aria-valuemax", steps);
+  var updateSteps = function(step) {
+    var percent = (parseInt(step) / steps) * 100;
+    $('#tutorial .progress-bar').css({width: percent + '%'}); $('#tutorial .progress-bar .step').text(step);
+    $('#tutorial .progress-bar .steps').text(steps);
+    $('#tutorial .progress-bar').attr("aria-valuenow", step);
+  };
   $('#tutorial .next').click(function(){
     var nextId = $(this).parents('#tutorial .tab-pane').next().attr("id");
     $('#tutorial [href="#'+nextId+'"]').tab('show');
@@ -8,10 +16,8 @@ $(function() {
   $('#tutorial a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     //update progress
     var step = $(e.target).data('step');
-    var percent = (parseInt(step) / 5) * 100;
-
-    $('#tutorial .progress-bar').css({width: percent + '%'});
-    $('#tutorial .progress-bar').text("Step " + step + " of 5");
-
+    updateSteps(step);
   })
+  updateSteps(1);
+  $('#tutorial').removeClass("hidden");
 });
